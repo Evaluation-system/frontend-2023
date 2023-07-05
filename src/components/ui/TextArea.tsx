858ce3341f4308e38207a6bfd06fff0bc4be0f98
Temerov Anton <hurdlelightsick@gmail.com>
2023-07-05 13:05:24 +0700

    Add Create Project Page Validation

---
+++ src/components/ui/TextArea.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+
+type Props = {
+  id: string;
+  placeholder: string;
+  errorMessage: any;
+  register: any;
+};
+
+const TextArea: FC<Props> = ({ id, placeholder, errorMessage, register }) => {
+  return (
+    <>
+      <textarea id={id} placeholder={placeholder} {...register} />
+      <p>{errorMessage}</p>
+    </>
+  );
+};
+
+export default TextArea;
--- src/pages/CreateProject.tsx
+++ src/pages/CreateProject.tsx
@@ -1,13 +1,17 @@
+import * as yup from "yup";
+import axios from "axios";
 import Button from "../components/ui/Button";
+import Input from "../components/ui/Input";
 import Modal from "../components/ui/Modal";
+import { addProject } from "../store/projects/projectSlice";
 import { FC, useEffect } from "react";
+import { instance } from "../api/axios.api";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { toast, Toaster } from "react-hot-toast";
 import { useAppDispatch, useAppSelector } from "../store/hooks/hooks";
-import { addProject } from "../store/projects/projectSlice";
 import { useNavigate } from "react-router";
-import axios from "axios";
-import { instance } from "../api/axios.api";
+import { yupResolver } from "@hookform/resolvers/yup";
+import TextArea from "../components/ui/TextArea";
 
 type Form = {
   title: string;
@@ -15,33 +19,34 @@ type Form = {
 };
 
 const CreateProject: FC = () => {
+  //Валидация
+  const schema = yup.object({
+    title: yup.string().required("Поле «Название» обязательно"),
+    description: yup.string().required("Поле «Описание» обязательно"),
+  });
+  //React-hook-form
   const {
     register,
     reset,
     handleSubmit,
     formState: { errors },
-  } = useForm<Form>();
+  } = useForm({ resolver: yupResolver(schema) });
 
-  const user = useAppSelector(state => state.user.user)
+  const user = useAppSelector((state) => state.user.user);
 
-  const dispatch = useAppDispatch();
   const navigate = useNavigate();
   // const url = "http://localhost:3005/"
 
-
-
   const onSubmit: SubmitHandler<Form> = (data) => {
-    const { title, description } = data
-    // dispatch(addProject(data));
+    const { title, description } = data;
     navigate("/profile");
     reset();
     const response = instance.post("http://localhost:3005/projects", {
       title: title,
       description: description,
       UserId: user?.id,
-    })
+    });
   };
-
 
   return (
     <Modal text="Создать проект">
@@ -50,20 +55,19 @@ const CreateProject: FC = () => {
         className="flex flex-col gap-[50px]"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <input
-          {...register("title", { required: "Поле «Название» обязательно" })}
+        <Input
+          id="title"
           placeholder="Название"
+          type="text"
+          register={{ ...register("title") }}
+          errorMessage={errors.title?.message}
         />
-        <textarea
-          {...register("description", {
-            required: "Поле «Описание» обязательно",
-          })}
+        <TextArea
+          id="description"
           placeholder="Описание"
+          register={{ ...register("description") }}
+          errorMessage={errors.description?.message}
         />
-        <span className="hidden">
-          {errors.title && toast.error(errors.title.message || "")}
-          {errors.description && toast.error(errors.description.message || "")}
-        </span>
         <div className="flex gap-5 justify-center xl:gap-[33px] xl:justify-end">
           <Button text="Назад" style="hidden xl:btn" action="/" />
           <button type="submit" className="btnGradient">
