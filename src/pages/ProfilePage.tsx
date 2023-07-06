6a9487215e893331591c0906d1a9d6a7193344d4
Anton Temerov <hurdlelightsick@gmail.com>
2023-07-06 20:29:15 +0700

    Delete svg, add react icon

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -9,6 +9,7 @@ import { logout } from "../store/user/userSlice";
 import { Toaster, toast } from "react-hot-toast";
 import { removeTokenFromLocalStorage } from "../helpers/localstorage.helper";
 import { NavLink } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
 
 const ProfilePage: FC = () => {
   //Для навигации
@@ -97,9 +98,7 @@ const ProfilePage: FC = () => {
 
           <div className="text-[#FFFFFF] opacity-50 flex flex-nowrap mb-3 gap-[5px] items-center">
             <p>{UserData?.email}</p>
-
             <BsDot />
-
             {/* Пока нет бэка */}
             <div>+79996669669</div>
 
@@ -112,18 +111,7 @@ const ProfilePage: FC = () => {
             onClick={() => navigate("/profile")}
           >
             Редактировать профиль
-            <svg
-              width="18"
-              height="17"
-              viewBox="0 0 18 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0 15.2862H18V16.56H0V15.2862ZM15.0429 4.45846C15.5571 3.94892 15.5571 3.18462 15.0429 2.67508L12.7286 0.382154C12.2143 -0.127385 11.4429 -0.127385 10.9286 0.382154L1.28571 9.936V14.0123H5.4L15.0429 4.45846ZM11.8286 1.27385L14.1429 3.56677L12.2143 5.47754L9.9 3.18462L11.8286 1.27385ZM2.57143 12.7385V10.4455L9 4.07631L11.3143 6.36923L4.88571 12.7385H2.57143Z"
-                fill="#4383FF"
-              />
-            </svg>
+            <AiOutlineEdit />
           </span>
           <button className="text-red" onClick={(): void => handleExitAccout()}>
             Выйти
